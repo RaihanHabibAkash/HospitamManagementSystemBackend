b57e441e6b02a588a2a9cdb2246036ef1126b98c
Raihan Habib Akash <raihanakash2003811@gmail.com>
2026-01-22 17:27:55 +0600

    Hospital Schema and model crated

---
+++ src/model/hospital.model.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const hospitalSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    address: [{
+        type: String,
+        required: true
+    }],
+    city: {
+        type: String,
+        required: true
+    },
+    pinCode: {
+        type: String,
+        required: true
+    },
+    specializationIn: {
+        type: String,
+        required: true
+    },
+    hotlineNumbers: [{
+        type: String,
+        required: true
+    }],
+    doctors: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Doctor",
+        required: true,
+        default: []
+    }],
+
+}, { timestamps: true });
+
+export const Hospital = mongoose.model("Hospital", hospitalSchema);
